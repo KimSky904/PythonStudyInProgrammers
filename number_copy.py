a6fe5eead699b53f8de87847224c92c0be0dac29
KimHaNeul <s2025@e-mirim.hs.kr>
2021-06-05 10:27:32 +0900

    0602

---
+++ number_copy.py
@@ -0,0 +1,13 @@
+my_name = 'Python' #문자열
+my_age = 25 #숫자
+print(my_name,'은 이제',my_age,'살')
+my_next_age = my_age+1
+print('내년에는',my_next_age,'살')
+
+multiply = 9*9
+divide = 30/5
+power = 2**10
+reminder = 15%4
+
+print(multiply,divide,power,reminder)
+
---
+++ variable.py
@@ -0,0 +1,3 @@
+identify = '지구인'
+number_of_legs=4
+print('안녕!나는',identify,'이야. 나는 다리가',number_of_legs,'개 있어.')
