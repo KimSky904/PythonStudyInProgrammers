c85979c6cfad80050a0456929678e64301d72e3f
KimHaNeul <s2025@e-mirim.hs.kr>
2021-06-06 23:59:39 +0900

    for문

---
+++ 0607/for.py
@@ -0,0 +1,15 @@
+patterns = ['가위','바위','보']
+
+for pattern in patterns: #변수의 내용이 하나씩 pattern에 들어감
+    print(pattern)
+
+    """
+    pattern = patterns[0]
+    print(pattern)
+    pattern = patterns[1]
+    print(pattern)
+    pattern = patterns[2]
+    print(pattern)
+    pattern = patterns[3]
+    print(pattern)
+    """
