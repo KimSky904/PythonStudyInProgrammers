d531be7f03a5a1d6bc89fc23872ac10475f59593
KimHaNeul <s2025@e-mirim.hs.kr>
2021-06-08 12:42:56 +0900

    module & url

---
+++ 0608/module.py
@@ -0,0 +1,5 @@
+import math
+r = 10
+print(2*math.pi*r)
+print(math.ceil(2.5))
+print(math.floor(2.5))
---
+++ 0608/random.py
@@ -0,0 +1,12 @@
+"""
+import Random
+candidates = ['가위','바위','보']
+selected = random.choice(candidates)
+print(selected)
+selected = random.choice(candidates)
+print(selected)
+selected = random.choice(candidates)
+print(selected)
+selected = random.choice(candidates)
+print(selected)
+"""
